b5fa47755ab3ebf96a4eb7c4ef28c5e2c0ec391c
Yusuf Mohsen <youssef.mohsen277@gmail.com>
2023-05-24 03:05:23 -0700

    header file

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*******Header files*******/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h> /* for strtok*/
+#include <unistd.h> /* for fork*/
+#include <sys/stat.h> /* stat the function*/
+#include <sys/types.h> /* for type pid*/
+#include <fcntl.h> /* for open files*/
+#include <errors.h> 
+#include <signal.h>
+#include <limits.h>
+
+/*******Header files*******/
+
