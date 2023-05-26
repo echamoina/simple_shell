e3b31f4322de86a981d344e6d8c590c7e65d9759
echamoina <echamoinaecha@gmail.com>
2023-05-26 11:14:30 -0700

    simple shell

--- builtinpp.c
+++ builtinpp.c
@@ -17,7 +17,6 @@ int _myhistory(info_t *info)
  * unset_alias - sets alias to string
  * @info: parameter struct
  * @str: the string alias
- *
  * Return: Always 0 on success, 1 on error
  */
 int unset_alias(info_t *info, char *str)
--- vars.c
+++ vars.c
@@ -72,7 +72,6 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 /**
  * replace_alias - replaces an aliases in the tokenized string
  * @info: the parameter struct
- *
  * Return: 1 if replaced, 0 otherwise
  */
 int replace_alias(info_t *info)
